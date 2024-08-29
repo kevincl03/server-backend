d9063567fba9579d8d0aa20e8d26fbe6363aa381
Kevin <kevinchanchi02@gmail.com>
2024-08-28 20:36:16 -0500

    28-08-2024

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http'); //importar modulo http
+
+const server = http.createServer((req, res) => {
+   //Establecer el codigo de estado y el tipo de contenido de la respuesta 
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/plain');
+   res.end('Hola, Mundo!\n'); //Enviar la respuesta
+});
+
+//Asignar un puerto y hacer que el servidor escuche en ese puerto
+const port = 3000;
+server.listen(port, () => {
+   console.log(`El servidor esta escuchando en http://localhost:${port}`);
+});
