fb680dcd17817458e6f38dc7311f40209cb911d2
Kevin <kevinchanchi02@gmail.com>
2024-09-04 20:39:36 -0500

    Carga-3

--- server-response-routes.js
+++ server-response-routes.js
@@ -3,12 +3,16 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Esta es la página principal</h1>');
-  } else if (req.url === '/contacto') {
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Esta es la página de "contactanos"');
+    res.end('Esta es la página principal');
+  } else if (req.url === '/html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Esta es la página con "Respuesta HTML"</h1>');
+  } else if (req.url === '/json') {
+    res.statusCode = 200;
+    res.setHeader('content-Type', 'application/json');
+    res.end(JSON.stringify({ message: "Respuesta del servidor en Json", statusCode: 200 } ));
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
